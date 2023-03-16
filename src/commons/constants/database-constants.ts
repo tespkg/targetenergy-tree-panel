fa08b62fe9c7427a5fd61a47be3398f39c6e0c99
Mostafa Zeinali <mostafa.zeinali@gmail.com>
2023-03-16 18:07:24 -0400

    Fix database constant values

--- src/commons/constants/database-constants.ts
+++ src/commons/constants/database-constants.ts
@@ -5,10 +5,10 @@ export const TYPE_DATABASE_LEVEL = 2
 export const CONTINENT_DATABASE_LEVEL = 3
 export const COUNTRY_DATABASE_LEVEL = 4
 // Region | Block | Production Station | Field | Reservoir | Well | Completion
-export const REGION_DATABASE_INDEX = 4
-export const BLOCK_DATABASE_INDEX = 5
-export const PRODUCTION_STATION_DATABASE_INDEX = 6
-export const FIELD_DATABASE_INDEX = 7
-export const RESERVOIR_DATABASE_INDEX = 8
-export const WELL_DATABASE_INDEX = 9
-export const COMPLETION_DATABASE_INDEX = 10
+export const REGION_DATABASE_INDEX = 5
+export const BLOCK_DATABASE_INDEX = 6
+export const PRODUCTION_STATION_DATABASE_INDEX = 7
+export const FIELD_DATABASE_INDEX = 8
+export const RESERVOIR_DATABASE_INDEX = 9
+export const WELL_DATABASE_INDEX = 10
+export const COMPLETION_DATABASE_INDEX = 11
