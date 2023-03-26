0a5c496c4c98ae4cb30e8bc56baaaf41fc60dc52
Hamed Rajabi <hamed.rajabi@targetofs.com>
2023-03-26 16:12:10 +0330

    Fix;Reverts constants database indices to its original

--- src/commons/constants/database-constants.ts
+++ src/commons/constants/database-constants.ts
@@ -4,11 +4,16 @@ export const COMPANY_DATABASE_LEVEL = 1
 export const TYPE_DATABASE_LEVEL = 2
 export const CONTINENT_DATABASE_LEVEL = 3
 export const COUNTRY_DATABASE_LEVEL = 4
+// We don't need the rest
+// export const REGION_DATABASE_LEVEL = 5
+// ...
+
 // Region | Block | Production Station | Field | Reservoir | Well | Completion
-export const REGION_DATABASE_INDEX = 5
-export const BLOCK_DATABASE_INDEX = 6
-export const PRODUCTION_STATION_DATABASE_INDEX = 7
-export const FIELD_DATABASE_INDEX = 8
-export const RESERVOIR_DATABASE_INDEX = 9
-export const WELL_DATABASE_INDEX = 10
-export const COMPLETION_DATABASE_INDEX = 11
+// It's zero based, they are indices
+export const REGION_DATABASE_INDEX = 4
+export const BLOCK_DATABASE_INDEX = 5
+export const PRODUCTION_STATION_DATABASE_INDEX = 6
+export const FIELD_DATABASE_INDEX = 7
+export const RESERVOIR_DATABASE_INDEX = 8
+export const WELL_DATABASE_INDEX = 9
+export const COMPLETION_DATABASE_INDEX = 10
